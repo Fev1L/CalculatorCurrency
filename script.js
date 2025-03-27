cbf7e5035072a09f381198d2db51b1e7da1353b4
Fev1L <demchvlad17@gmail.com>
2025-03-27 20:03:57 +0100

    Final Version

--- script.js
+++ script.js
@@ -1,74 +1,116 @@
+let isCurrencyAll = false;
+
 function submitClick(){
     let enter = document.getElementById("enter").value
     let result = 0
     let value = 0
 
     let currency1 = document.getElementById("enter-currency").value
-    let currency2 = document.getElementById("result-currency").value
-
+    let currency2 = document.querySelectorAll("#result-currency").value
+    
     if(currency1 == currency2){
-        alert("Similar")
+        alert("You enter the same valut!")
         return
     }
 
     if(enter < 0){
-        alert("Negative")
+        alert("You can’t enter negative value")
         return
     }
 
     if(enter == 0){
-        alert("NULL")
+        alert("You can’t enter zero")
         return
     }
 
-    switch(currency1){
-        case "usd":
-            value = enter
-            break
-        case "eur":
-            value = enter * 1.08
-            break
-        case "gbp":
-            value = enter * 1.29
-            break
-        case "pln":
-            value = enter * 0.26
-            break
-        case "krw":
-            value = enter * 0.00068
-            break
-        case "cny":
-            value = enter * 0.14
-            break
-    }
+    let rates = {
+        "usd": 1, "eur": 1.08, "gbp": 1.29, 
+        "pln": 0.26, "krw": 0.00068, "cny": 0.14
+    };
+    value = enter * rates[currency1];
 
-    switch(currency2){
-        case "usd":
-            result = value * 1
-            break
-        case "eur":
-            result = value * 0.92
-            break
-        case "gbp":
-            result = value * 0.77
-            break
-        case "pln":
-            result = value * 3.87
-            break
-        case "krw":
-            result = value * 1467.40
-            break
-        case "cny":
-            result = value * 7.23
-            break
+    if(isCurrencyAll == false){
+        let exchange = {
+            "usd": 1, "eur": 0.92, "gbp": 0.77, 
+            "pln": 3.87, "krw": 1467.40, "cny": 7.23
+        };
+        result = value * exchange[document.querySelectorAll("#result-currency")[0].value];
+        document.getElementById("result").value = result;
+    }else{
+        switch(currency1){
+            case "usd":
+                document.querySelectorAll("#result-currency")[2].value = "eur"
+                document.querySelectorAll("#result-currency")[1].value = "gbp"
+                document.querySelectorAll("#result-currency")[3].value = "pln"
+                document.querySelectorAll("#result-currency")[4].value = "krw"
+                document.querySelectorAll("#result-currency")[5].value = "cny"
+                break
+            case "eur":
+                document.querySelectorAll("#result-currency")[2].value = "usd"
+                document.querySelectorAll("#result-currency")[1].value = "gbp"
+                document.querySelectorAll("#result-currency")[3].value = "pln"
+                document.querySelectorAll("#result-currency")[4].value = "krw"
+                document.querySelectorAll("#result-currency")[5].value = "cny"
+                break
+            case "gbp":
+                document.querySelectorAll("#result-currency")[2].value = "eur"
+                document.querySelectorAll("#result-currency")[1].value = "usd"
+                document.querySelectorAll("#result-currency")[3].value = "pln"
+                document.querySelectorAll("#result-currency")[4].value = "krw"
+                document.querySelectorAll("#result-currency")[5].value = "cny"
+                break
+            case "pln":
+                document.querySelectorAll("#result-currency")[2].value = "eur"
+                document.querySelectorAll("#result-currency")[1].value = "gbp"
+                document.querySelectorAll("#result-currency")[3].value = "usd"
+                document.querySelectorAll("#result-currency")[4].value = "krw"
+                document.querySelectorAll("#result-currency")[5].value = "cny"
+                break
+            case "krw":
+                document.querySelectorAll("#result-currency")[2].value = "eur"
+                document.querySelectorAll("#result-currency")[1].value = "gbp"
+                document.querySelectorAll("#result-currency")[3].value = "pln"
+                document.querySelectorAll("#result-currency")[4].value = "usd"
+                document.querySelectorAll("#result-currency")[5].value = "cny"
+                break
+            case "cny":
+                document.querySelectorAll("#result-currency")[2].value = "eur"
+                document.querySelectorAll("#result-currency")[1].value = "gbp"
+                document.querySelectorAll("#result-currency")[3].value = "pln"
+                document.querySelectorAll("#result-currency")[4].value = "krw"
+                document.querySelectorAll("#result-currency")[5].value = "usd"
+                break
+        }
+        
+        for(let i = 1;i<6;i++){
+            let exchange = {
+                "usd": 1, "eur": 0.92, "gbp": 0.77, 
+                "pln": 3.87, "krw": 1467.40, "cny": 7.23
+            };
+            result = value * exchange[document.querySelectorAll("#result-currency")[i].value];
+            document.querySelectorAll("#result")[i].value = result;
+        }   
     }
-    enter = document.getElementById("enter").value
-    document.getElementById("result").value = result;
+    
 }
 
 function clearClick(){
     document.getElementById("enter").value = 0
-    document.getElementById("result").value = 0
+    for(let i = 0;i<6;i++){
+        document.querySelectorAll("#result")[i].value = 0
+    }
+    
     enter = 0
     result = 0
+}
+
+function currencyClick(){
+    const div1 = document.querySelector(".one-currency");
+    const div2 = document.querySelector(".all-currency");
+
+    isCurrencyAll = !isCurrencyAll;
+    
+    div1.style.display = isCurrencyAll ? "none" : "flex";
+    div2.style.display = isCurrencyAll ? "flex" : "none";
+    document.getElementById("edit-currency").textContent = isCurrencyAll ? "One currency" : "All currency";
 }
